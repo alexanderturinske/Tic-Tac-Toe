99942e40447de9d8046dbb65ad7c5036641f32ae
Alexander James Turinske <a.turinske@gmail.com>
2016-06-02 16:51:28 -0700

    Build a server to serve static files

---
+++ server/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
+
+var app = express();
+
+var port = process.env.PORT || 1337;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.listen(port, function() {
+  if (err) {
+    console.log('Server not started!')
+  }
+  console.log('The server has started on port ' + port);
+})
