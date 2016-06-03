2a192e7e1eda542c4ffbb68c238d525b7937475b
Alexander James Turinske <a.turinske@gmail.com>
2016-06-02 17:16:38 -0700

    Fix bug where err wasn't defined

--- server/server.js
+++ server/server.js
@@ -12,9 +12,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
 
-app.listen(port, function() {
+app.listen(port, function(err) {
   if (err) {
     console.log('Server not started!');
   }
   console.log('The server has started on port ' + port);
-})
+});
