3348ab68a97091835094b4ced1293090f6c62387
Alexander James Turinske <a.turinske@gmail.com>
2016-06-03 20:07:23 -0700

    Add grid to state and import board

--- client/app.jsx
+++ client/app.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-
+import Board from './board.jsx';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      grid: ['', '', '', '', '', '', '', '', ''],
     };
   }
 
   render() {
     return (
       <div>
-        "Tic-Tac-Toe"
+        <Board grid={this.state.grid} />
       </div>
     );
   }
