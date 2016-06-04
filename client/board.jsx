a5b84398a49f349144177f05657a77c2370815af
Alexander James Turinske <a.turinske@gmail.com>
2016-06-03 20:06:37 -0700

    Create board element and map squares in it

---
+++ client/board.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Square from './square.jsx';
+
+class Board extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div id='board'>
+        {this.props.grid.map((square, i) =>
+          <Square key={i} />
+        )}
+      </div>
+    );
+  }
+}
+
+Board.propTypes = {
+  grid: React.PropTypes.array,
+};
+
+export default Board;
