7e881e33d1d71599d45b6a9dcae1b4a3764b51c2
Alexander James Turinske <a.turinske@gmail.com>
2016-06-04 08:56:10 -0700

    Build function to change square when clicked and winning logic
    Winning logic only checks for the top row at the moment.

--- client/app.jsx
+++ client/app.jsx
@@ -10,6 +10,21 @@ class App extends Component {
     };
   }
 
+  handleTurn(number) {
+    this.setState({
+      grid: grid.map((e, i) => {
+        if (i === number) {
+          return 'X';
+        } else {
+          return e;
+        }
+      }),
+    });
+    if (this.state.grid[0] === 'X' && this.state.grid[1] === 'X' && this.state.grid[2] === 'X') {
+      alert('You have won!');
+    }
+  }
+
   render() {
     return (
       <div>
