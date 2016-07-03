77e22d8bbc2dc381662da6e530a8974c501fbc95
Alexander James Turinke <a.turinske@gmail.com>
2016-07-03 16:49:19 -0700

    Fix bug where checkForWinner needed an array passed

--- client/angular/app/app.js
+++ client/angular/app/app.js
@@ -7,10 +7,12 @@
   'use strict';
 
   angular
-    .module('TicTacToe', [])
+    .module('TicTacToe', ['logic'])
     .controller('BoardController', BoardController);
 
-  function BoardController() {
+  BoardController.$inject = ['WinnerService'];
+
+  function BoardController(WinnerService) {
     var vm = this;
     vm.squares = ['', '', '', '', '', '', '', '', ''];
     vm.next = 'X';
@@ -18,40 +20,15 @@
       if (!vm.squares[index]) {
         if (vm.next === 'X') {
           vm.squares[index] = 'X';
-          vm.checkForWinner(vm.next);
+          WinnerService.checkForWinner(vm.squares, vm.next);
           vm.next = 'O';
         } else {
           vm.squares[index] = 'O';
-          vm.checkForWinner(vm.next);
+          WinnerService.checkForWinner(vm.squares, vm.next);
           vm.next = 'X';
         }
       } else {
         alert('This position has been used!');
-      }
-    };
-    vm.checkForWinner = function checkForWinner(test) {
-      // vertical check
-      if (vm.squares[0] === test && vm.squares[1] === test && vm.squares[2] === test ||
-        vm.squares[3] === test && vm.squares[4] === test && vm.squares[5] === test ||
-        vm.squares[6] === test && vm.squares[7] === test && vm.squares[8] === test
-      ) {
-        alert('Player ' + test + ' has won!');
-        return 'Player ' + test + ' has won!';
-      }
-      // horizontal check
-      if (vm.squares[0] === test && vm.squares[3] === test && vm.squares[6] === test ||
-        vm.squares[1] === test && vm.squares[4] === test && vm.squares[7] === test ||
-        vm.squares[2] === test && vm.squares[5] === test && vm.squares[8] === test
-      ) {
-        alert('Player ' + test + ' has won!');
-        return 'Player ' + test + ' has won!';
-      }
-      // cross check
-      if (vm.squares[0] === test && vm.squares[4] === test && vm.squares[8] === test ||
-        vm.squares[2] === test && vm.squares[4] === test && vm.squares[6] === test
-      ) {
-        alert('Player ' + test + ' has won!');
-        return 'Player ' + test + ' has won!';
       }
     };
     vm.resetGrid = function resetGrid() {
