ff0a1c29334c96ebb767e1025eff7e0054f3aa23
Alexander James Turinke <a.turinske@gmail.com>
2016-07-03 15:36:55 -0700

    Implement reset and checkForWinner methods

--- client/angular/app/app.js
+++ client/angular/app/app.js
@@ -27,5 +27,33 @@
         alert('This position has been used!');
       }
     };
+    vm.checkForWinner = function checkForWinner(test) {
+      // vertical check
+      if (vm.squares[0] === test && vm.squares[1] === test && vm.squares[2] === test ||
+        vm.squares[3] === test && vm.squares[4] === test && vm.squares[5] === test ||
+        vm.squares[6] === test && vm.squares[7] === test && vm.squares[8] === test
+      ) {
+        alert('Player ' + test + ' has won!');
+        return 'Player ' + test + ' has won!';
+      }
+      // horizontal check
+      if (vm.squares[0] === test && vm.squares[3] === test && vm.squares[6] === test ||
+        vm.squares[1] === test && vm.squares[4] === test && vm.squares[7] === test ||
+        vm.squares[2] === test && vm.squares[5] === test && vm.squares[8] === test
+      ) {
+        alert('Player ' + test + ' has won!');
+        return 'Player ' + test + ' has won!';
+      }
+      // cross check
+      if (vm.squares[0] === test && vm.squares[4] === test && vm.squares[8] === test ||
+        vm.squares[2] === test && vm.squares[4] === test && vm.squares[6] === test
+      ) {
+        alert('Player ' + test + ' has won!');
+        return 'Player ' + test + ' has won!';
+      }
+    };
+    vm.resetGrid = function resetGrid() {
+      vm.squares = ['', '', '', '', '', '', '', '', ''];
+    };
   }
 })();
