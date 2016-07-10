8ea68220c9ecdd6ceef475b1f9db401839e9cfe1
Alexander James Turinke <a.turinske@gmail.com>
2016-07-10 14:00:23 -0700

    Implement game logic and update current player

--- client/vanilla/tictactoe.js
+++ client/vanilla/tictactoe.js
@@ -5,14 +5,43 @@ var board = document.querySelector('#board');
 // show current
 document.querySelector('.current').innerHTML = current;
 
+// define function for game logic
+var checkForWinner = function (array, test) {
+  // vertical check
+  if (array[0].innerHTML === test && array[1].innerHTML === test && array[2].innerHTML === test ||
+    array[3].innerHTML === test && array[4].innerHTML === test && array[5].innerHTML === test ||
+    array[6].innerHTML === test && array[7].innerHTML === test && array[8].innerHTML === test
+  ) {
+    alert('Player ' + test + ' has won!');
+    return 'Player ' + test + ' has won!';
+  }
+  // horizontal check
+  if (array[0].innerHTML === test && array[3].innerHTML === test && array[6].innerHTML === test ||
+    array[1].innerHTML === test && array[4].innerHTML === test && array[7].innerHTML === test ||
+    array[2].innerHTML === test && array[5].innerHTML === test && array[8].innerHTML === test
+  ) {
+    alert('Player ' + test + ' has won!');
+    return 'Player ' + test + ' has won!';
+  }
+  // cross check
+  if (array[0].innerHTML === test && array[4].innerHTML === test && array[8].innerHTML === test ||
+    array[2].innerHTML === test && array[4].innerHTML === test && array[6].innerHTML === test
+  ) {
+    alert('Player ' + test + ' has won!');
+    return 'Player ' + test + ' has won!';
+  }
+};
+
 // define function to handle turns
 function handleTurn() {
   this.innerHTML = current;
+  checkForWinner(divs, current);
   if (current === 'X') {
     current = 'O';
   } else {
     current = 'X';
   }
+  document.querySelector('.current').innerHTML = current;
 }
 
 // create squares
